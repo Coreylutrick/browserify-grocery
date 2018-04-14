8d1c41ce71f1ba32df26fc75706bf1840fb3c179
Corey Lutrick <coreylutrick@gmail.com>
2018-04-14 11:59:28 -0700

    did enter nd leave events

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,12 +1,14 @@
 const outPutDiv = document.getElementById('departments');
 
+const events = require('./events');
+
 const domString = (departments) =>
 {
   let strang = '';
   departments.forEach((departments) =>
   {
     strang +=   `<div class='department col-sm-3'>`;
-    strang +=     `<h3 class='hide'>${departments.name}</h3>`;
+    strang +=     `<h3 class='hide department-title' data-department-id='${departments.id}'>${departments.name}</h3>`;
     strang +=     `<img class='dep-img' src='${departments.img}'>`;
     strang +=   `</div>`;
   });
@@ -16,6 +18,7 @@ const domString = (departments) =>
 const printToDom = (DepArray) =>
 {
   outPutDiv.innerHTML = domString(DepArray);
+  events.addDepartmentEvents();
 };
 
 module.exports = printToDom;
