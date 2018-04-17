c2b40d59f534762a02e979227653f4be5c910b0b
Corey Lutrick <coreylutrick@gmail.com>
2018-04-16 19:47:18 -0700

    added panels and styled items

--- javascripts/itemsDom.js
+++ javascripts/itemsDom.js
@@ -5,7 +5,18 @@ const itemsDomString = (items) =>
   let domString = '';
   items.forEach((items) =>
   {
-    domString = `<h3>${items.name}</h3>`;
+    domString +=  `<div class='col-sm-3 text-center item'>`;
+    domString +=    `<div class="panel panel-info">`;
+    domString +=      `<div class="panel-heading">`;
+    domString +=        `<h3 class="panel-title">${items.name}</h3>`;
+    domString +=      `</div>`;
+    domString +=      `<div class="panel-body">`;
+    domString +=        `<div class='row'>`;
+    domString +=          `<img class='itemImg' src='${items.img}'>`;
+    domString +=        `</div>`;
+    domString +=      `</div>`;
+    domString +=    `</div>`;
+    domString +=  `</div>`;
   });
   return domString;
 };
